b1d97bee8e06726ac3215a542570bef6b3a08113
FelipeRicardo5 <felipe1ricardo158@gmail.com>
2025-10-24 17:18:44 -0300

    feat: implementa rotas de 404

--- payments/pix.py
+++ payments/pix.py
@@ -10,6 +10,6 @@ class Pix():
         hash_payment = f'hash_payment_{bank_payment_id}'
         
         img = qrcode.make(hash_payment)
-        img.save("static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
 
         return {"bank_payment_id": bank_payment_id, "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
